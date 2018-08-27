adcffa5de79b79f886c8941f7750b6f7fc575be9
Anna-Mariia Paitak <39438328+annpaytak@users.noreply.github.com>
2018-08-27 10:40:57 +0300

    minor changes in syntax

--- FE_9_8_homework_functions/homework/src/isPrime.js
+++ FE_9_8_homework_functions/homework/src/isPrime.js
@@ -1,9 +1,11 @@
  function isPrime(integer) {
-            if (integer == 1)
+            if (integer === 1){
                 return false;
-            for (var d = 2; d * d <= integer; d++) {
-                if (integer % d == 0)
+            }
+            for (let d = 2; d * d <= integer; d++) {
+                if (integer % d === 0){
                     return false;
+                }
             }
             return true;
         }
